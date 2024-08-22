957ed4c4c2521cb7010318f44229313a7f36fdea
Delplanque <benjamin.delplanque@hotmail.fr>
2024-08-22 14:42:27 +0200

    Exercice Symfony

--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -4,8 +4,10 @@ namespace App\Form;
 
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\OptionsResolver; /*Pour pouvoir utiliser configureOption*/
+use Symfony\Component\Form\Extension\Core\Type\SubmitType; /*Utilisation du SubmitType*/
 
 class ContactFormType extends AbstractType
 {
@@ -14,14 +16,23 @@ class ContactFormType extends AbstractType
         $builder
             ->add('objet')
             ->add('email')
-            ->add('message')
+
+             //On a rajouté un label et on a rendu le champ optionnel en
+            // donnant la valeur false à l'attribut required
+            ->add('message', TextareaType::class, [
+                'label' => 'Votre message',
+                'required' => false
+            ]
+        )
+            ->add('save', SubmitType::class, [ /*besoin de use Symfony\Component\Form\Extension\Core\Type\SubmitType;*/
+                'label' => 'Envoyer le message'])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Contact::class,
+            'data_class' => Contact::class, /*lien avec l'entité Contact (Contact.php), donc lien à la base de données*/
         ]);
     }
 }
