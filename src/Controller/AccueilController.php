52631ffab4afae21270e30cea56d635d58cf2536
Delplanque <benjamin.delplanque@hotmail.fr>
2024-08-26 16:42:03 +0200

    Exercice Disque Symfony

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -4,9 +4,13 @@ namespace App\Controller;
 
 use App\Repository\ArtistRepository;
 use App\Repository\DiscRepository;
+/*use Doctrine\ORM\EntityManagerInterface;*/
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+
+/*Le EntityManagerInterface $em n'est pas utilisé pour le moment. C'est sensé afficher seulement les artistes contenant "Neil", mais je n'en ai pas inclus dans
+ la base de données pour le moment*/
 
 class AccueilController extends AbstractController
 {
@@ -17,11 +21,13 @@ class AccueilController extends AbstractController
 
     private $artistRepo;
     private $discRepo;
+    /*private $em;*/
 
-    public function __construct(ArtistRepository $artistRepo, DiscRepository $discRepo)
+    public function __construct(ArtistRepository $artistRepo, DiscRepository $discRepo, /*EntityManagerInterface $em*/)
     {
         $this->artistRepo = $artistRepo;
         $this->discRepo = $discRepo;
+        /*$this->em = $em;*/
     }
 
     #[Route('/accueil', name: 'app_accueil')]
@@ -30,6 +36,9 @@ class AccueilController extends AbstractController
 
          //on appelle la fonction `findAll()` du repository de la classe `Artist` afin de récupérer tous les artists de la base de données;
         $artistes = $this->artistRepo->findAll();
+        
+        //$artistes = $this->artistRepo->getSomeArtists("Neil"); /* //on appelle le repository pour accéder à la fonction */
+        //dd($artistes); /* on teste le contenu de la variable $artistes : dd() veut dire Dump and Die (ça permet d'afficher du texte à l'écran et de terminer l'exécution du programme.) */
 
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
--- src/Repository/ArtistRepository.php
+++ src/Repository/ArtistRepository.php
@@ -31,6 +31,44 @@ class ArtistRepository extends ServiceEntityRepository
 //        ;
 //    }
 
+
+// Version avec le DQL
+
+/*public function getSomeArtists($name)
+{
+    $entityManager = $this->getEntityManager();
+
+    $query = $entityManager->createQuery(
+        'SELECT a 
+        FROM App\Entity\Artist a
+        WHERE a.name LIKE :name'
+    )->setParameter('name', '%'.$name.'%');
+
+    return $query->getResult();
+}*/
+
+// Version avec le QueryBuilder :
+
+public function getSomeArtists($name)
+{
+    $qb = $this->createQueryBuilder('a'); /* $this->createQueryBuilder est une méthode qui crée une nouvelle instance d'un objet Doctrine QueryBuilder. */
+                                          /* L'argument « a » est un alias pour l'entité interrogée, qui est une entité Artist. */
+                                          /*L'objet QueryBuilder est stocké dans la variable $qb.*/
+    $qb                                   
+        ->andWhere('a.name like :name')             /* andWhere est une méthode qui ajoute une clause conditionnelle à la requête. */
+                                                    /* La clause est a.name comme :name, ce qui signifie "trouver les artistes dont la colonne name contient la valeur du paramètre :name". */
+                                                    /* Le paramètre :name est un placeholder qui sera remplacé par la valeur réelle transmise à la méthode. */
+        ->setParameter('name', '%'.$name.'%')       /* setParameter est une méthode qui définit la valeur d'un paramètre dans la requête. */
+                                                    /* Le paramètre :name est défini sur la valeur de l'argument $name, entouré de caractères génériques %. Cela permet à la requête d'effectuer une recherche LIKE, faisant correspondre les artistes dont les noms contiennent la valeur spécifiée. */
+        ->orderBy('a.id', 'ASC')                    /* orderBy est une méthode qui spécifie l'ordre de tri des résultats. */
+                                                    /* La requête triera les résultats par la colonne id de l'entité Artist par ordre croissant (ASC). */
+        ->setMaxResults(10);                        /* setMaxResults est une méthode qui limite le nombre de résultats renvoyés par la requête. Dans ce cas, la requête renverra au maximum 10 résultats. */
+
+    $query = $qb->getQuery();                       /* getQuery est une méthode qui renvoie l'objet Query construit à partir de QueryBuilder. */
+    $artists = $query->getResult();                 /* getResult est une méthode qui exécute la requête et renvoie les résultats sous forme de tableau d'objets. */
+     return $artists;                               /* La méthode renvoie le tableau d'objets Artist qui correspondent au nom spécifié. */
+}
+
 //    public function findOneBySomeField($value): ?Artist
 //    {
 //        return $this->createQueryBuilder('a')
