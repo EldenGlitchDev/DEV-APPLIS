9dab4fb978a23495935d93dc416fb0f2a188546e
Delplanque <benjamin.delplanque@hotmail.fr>
2024-08-28 15:52:58 +0200

    Exercices disque Symfony

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -32,11 +32,56 @@ class ContactController extends AbstractController
                 return $this->redirectToRoute('app_accueil');
             }
 
-
-
         return $this->render('contact/index.html.twig', [
             'form' => $form->createView(), /* méthode createView() pour générer une représentation du formulaire prête à être affichée dans notre template. */
             'form' => $form
         ]);
     }
 }
+
+/* Exemple service en Symfony */
+
+/*namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Form\DemoFormType;
+use App\Form\ContactFormType;
+use App\Service\MailService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(Request $request, EntityManagerInterface $entityManager, MailerInterface $mailer, MailService $ms): Response
+    {
+        $form = $this->createForm(ContactFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            //on crée une instance de Contact
+            $message = new Contact();
+            // Traitement des données du formulaire
+            //...
+            //persistance des données
+
+            $entityManager->persist($message);
+            $entityManager->flush();
+
+            //envoi de mail avec notre service MailService
+            $email = $ms->sendMail('hello@example.com', $message->getEmail(), $message->getObjet(), $message->getMessage() );
+//            dd($message->getEmail());
+
+        }
+        
+    }
+}*/
+
