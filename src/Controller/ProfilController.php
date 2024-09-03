512fbab8d591623cc7e1025d05f4517e1c771ab1
Delplanque <benjamin.delplanque@hotmail.fr>
2024-09-03 14:51:51 +0200

    Exercice Sympfony

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -5,13 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\UtilisateurRepository;
 
 class ProfilController extends AbstractController
 {
+
+    private $userRepo;
+    public function __construct(UtilisateurRepository $userRepo){
+        $this->userRepo = $userRepo;
+    }
+
     #[Route('/profil', name: 'app_profil')]
     public function index(): Response
     {
-        $info = ['lastname' => 'Loper', 'firstname' => 'Dave', 'email' => 'daveloper@code.dom', 'birthdate' => '01/01/1970'];
+        $identifiant = $this->getUser()->getUserIdentifier();
+        if($identifiant){
+            $info = $this->userRepo->findOneBy(["email" => $identifiant]);
+        }
 
         return $this->render('profil/index.html.twig', [
             'informations' => $info
